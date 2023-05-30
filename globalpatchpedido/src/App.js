e8f33eb968842a5a62e002dc3378088d0919f47d
M4rrt <guilhermemartinszot@gmail.com>
2023-05-29 22:53:58 -0300

    linha

--- globalpatchpedido/src/App.js
+++ globalpatchpedido/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Linha from "./components/linha/linha.component.jsx"
 
@@ -6,17 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
         <Linha  
           classNames={["bgg","bgg","bgg","bgg2","bgsg","bgsg"]} 
           produto=""
